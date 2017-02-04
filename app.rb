f8bcd4df4a967dd23f2676b0c7add036af4b25c2
kevinah <kevinah13@gmail.com>
2017-02-03 23:09:52 -0500

    setup webhook

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'sinatra'
 
 # Talk to Facebook
 get '/webhook' do
-  params['hub.challenge'] if ENV["VERIFY_TOKEN"] == params['hub.verify_token']
+  params['hub.challenge']
 end
 
 get "/" do
-  "Nothing to see here"
+  params['hub.challenge']
 end
